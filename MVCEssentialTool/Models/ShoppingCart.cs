6571b121f4093cd59e5deea997f466604592a91d
Sandeep Singh <contact.sandeep2004@gmail.com>
2016-07-13 16:22:44 -0400

    last commit of the dat

---
+++ MVCEssentialTool/Models/ShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCEssentialTool.Models
+{
+    public class ShoppingCart
+    {
+        private IValueCalculator calc;
+
+        public ShoppingCart(IValueCalculator c)
+        {
+            calc = c;
+        }
+
+        public IEnumerable<Product> Products { get; set; }
+
+        public decimal CalculateProductTotal()
+        {
+            return calc.ValueProducts(Products);
+        }
+    }
+}
